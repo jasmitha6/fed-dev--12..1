d120ebe4aa659702dde77ec0be5c8557e7144ecc
jasmitha <jasmieyjas@gmail.com>
2018-02-03 23:05:11 +0530

    Add files via upload

---
+++ ass-12.1/src/app/routing/routing.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserDataComponent } from 'app/user-data/user-data.component';
+// defining routes
+
+const routes: Routes = [
+];
+
+
+export const routedComponents = RouterModule.forRoot(routes)
---
+++ ass-12.1/src/app/user-data/user-data.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserDataComponent } from './user-data.component';
+// its default unit testing file for checking thet whether our component is working correctly or not.
+describe('UserDataComponent', () => {
+  let component: UserDataComponent;
+  let fixture: ComponentFixture<UserDataComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserDataComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserDataComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
